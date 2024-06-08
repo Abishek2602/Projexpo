78f258158685f677f7d7d964234d231f497ec060
Abishek2602 <abishekm@student.tce.edu>
2024-06-08 10:20:15 +0530

    Title added

--- class1/src/components/Home.jsx
+++ class1/src/components/Home.jsx
@@ -13,7 +13,11 @@ const Home = () => {
 
   return (
     <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center py-28">
-      <div className="w-full" data-aos="fade-up"> 
+      <div className='py-24 flex flex-col gap-24' data-aos='fade-down'>
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better Data</span> Scalable AI.</h1>
+        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+      </div>
+      <div className="w-full py-28" data-aos="fade-up"> 
         <Carousel />
       </div>
       <div className="w-full mt-8 px-16 py-20" data-aos="fade-up">
